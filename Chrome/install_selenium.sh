2736214c91f898edcecb713b8e9a45f0616a5693
Norbaeocystin <baranrastislav@gmail.com>
2019-04-07 09:03:03 +0200

    Update install_selenium.sh
    no spaces between variables

--- Chrome/install_selenium.sh
+++ Chrome/install_selenium.sh
@@ -1,5 +1,5 @@
 #!/bin/bash
-requirements = "libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 chromium-browser unzip"
+requirements="libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 chromium-browser unzip"
 function install()
 {
  apt-get install $1

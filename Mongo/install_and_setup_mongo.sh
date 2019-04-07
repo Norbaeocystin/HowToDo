96f0040078aedf09b0f476b70af7acdd46f8d0ab
Norbaeocystin <baranrastislav@gmail.com>
2019-04-07 09:45:04 +0200

    Update install_and_setup_mongo.sh

--- Mongo/install_and_setup_mongo.sh
+++ Mongo/install_and_setup_mongo.sh
@@ -1,8 +1,13 @@
-function testMongoScript {
-    mongo <<EOF
-    use mydb
-    db.leads.findOne()
-    db.leads.find().count()
+#!/bin/bash
+#read password - password by input
+password="yourpassword"
+username="yourusername"
+create_user='db.createUser( { user: "Admin", pwd: "password", roles: [ { role: "root", db: "admin" } ] } )'
+create_user=${create_user/"password"/$password}
+create_user=${create_user/"password"/$user}
+#function which start mongo shell and parse command create_user
+function user_creation {
+    mongo --host 192.168.100.4 <<EOF
+    $create_user
 EOF
 }
-also possibility to use mongo with --eval flag

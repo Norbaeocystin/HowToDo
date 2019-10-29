5967cc0911ddc0e78b882a1d3213ca0b15dd9f4e
Norbaeocystin <baranrastislav@gmail.com>
2019-10-29 15:14:10 +0100

    Update install_selenium.sh

--- Chrome/install_selenium.sh
+++ Chrome/install_selenium.sh
@@ -9,7 +9,7 @@ for requirement in $requirements
 do
   install "$requirement"
 done
-wget https://chromedriver.storage.googleapis.com/2.46/chromedriver_linux64.zip
+wget https://chromedriver.storage.googleapis.com/77.0.3865.40/chromedriver_linux64.zip
 unzip chromedriver_linux64.zip
 mv -f ~/chromedriver /usr/local/bin/chromedriver
 chown root:root /usr/local/bin/chromedriver

fb1b64ccd2cd8de985f62e14736f04b64f5d2396
Norbaeocystin <baranrastislav@gmail.com>
2019-11-23 10:36:23 +0100

    Update install_selenium.sh

--- Chrome/install_selenium.sh
+++ Chrome/install_selenium.sh
@@ -2,7 +2,7 @@
 requirements="libglib2.0-0 libnss3 libgconf-2-4 libfontconfig1 chromium-browser unzip"
 function install()
 {
- apt-get install $1
+ yes | apt-get install $1
 }
 apt-get update
 for requirement in $requirements
